13eb83823164bc8ddf3e0c9e90f8f5292fca1aaa
Michael Harris <xero5k@yahoo.com>
2023-01-14 09:32:11 -0600

    Added basic CRUD

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,26 @@
 class ListsController < ApplicationController
+
+  def index
+    render json: List.all
+  end 
+
+  def create
+    list = List.create(name: params[:name])
+    render json: list, status: :created
+  end 
+
+  def update
+    list = List.find_by(id: params[:id])
+    if list 
+      list.update(name: params[:name])
+    else
+      render json: {errors: list.errors.full_messages}, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    list = List.find_by(id: params[:id])
+    list.destroy
+  end 
+
 end

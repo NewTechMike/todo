58d85312eaed33cc2e11a70023040ecd655b0349
Michael Harris <xero5k@yahoo.com>
2023-01-17 07:46:02 -0600

    Added User Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_145534) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_130820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

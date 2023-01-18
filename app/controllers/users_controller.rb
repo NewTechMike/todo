f639e3bb146ecce54367a624ea225851ec0b6684
Michael Harris <xero5k@yahoo.com>
2023-01-18 09:37:41 -0600

    Added show for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user
+    else
+      render json: { errors: "Not Authorizied"}, status: :unauthorized
+    end 
+  end
 end

892b2ae15fc37c71920eac58b9ac3a1da7624353
Michael Harris <xero5k@yahoo.com>
2023-01-24 14:48:14 -0600

    Added Task Update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,11 +10,18 @@ class TasksController < ApplicationController
     if task
       render json: task, status: :created
     else
-      render json: { error: task.errors:full_messages}, status: :unprocessable_entity
+      render json: { error: task.errors:full_messages }, status: :unprocessable_entity
     end 
   end
 
   def update
+    task = Task.find_by(title: params[:title])
+    if title
+      task.update(title: params[:title])
+      render json: task, status: :updated
+    else 
+      render json: { error: "Task not Found" }, status: :not_found
+    end 
   end
 
   def destroy

07ab047a4b045de63d905eba998492275100c7d0
Michael Harris <xero5k@yahoo.com>
2023-02-15 13:37:54 -0600

    Added Sessions Store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
+      render json: user, status: :created
     else
       render json: { error: "Invalid name or password"}, status: :unauthorized
     end 

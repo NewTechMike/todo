a64d58ae077048595c3a8791c6b994099246c58e
Michael Harris <xero5k@yahoo.com>
2023-03-05 19:02:11 -0600

    minor change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,6 @@ class TasksController < ApplicationController
 
   def this_list
     user = current_user
-    byebug
     return list = current_user.lists.find_by(id: params[:list_id])
   end 
 end

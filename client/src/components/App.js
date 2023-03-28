01693c65e62b8fdf0c8768e8b94c72fe6c71b534
Michael Harris <xero5k@yahoo.com>
2023-03-28 07:29:42 -0500

    Fixing browserrouter errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Welcome from "./Welcome";

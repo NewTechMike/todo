0bd9dcf9fd27689358054f5980efdb113b885423
Michael Harris <xero5k@yahoo.com>
2023-04-21 07:37:27 -0500

    Updated login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(){
+  const [username, setUsername] = useState("");
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log("submit was triggered")
+  }
 
   return(
     <div>
       <h1> Login Page </h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username" style={{color: "dark blue"}}>
+          UserName
+        </label>
+        <input  
+          type="text"
+          id="username"
+          value={username}
+          autoComplete="off"
+          onChange={(e) => setUsername(e.target.value)}/>
+
+      </form>
     </div>
   )
 }

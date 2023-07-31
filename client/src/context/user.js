6eae3a54a2ede6f99618836009d5fa08ce4a624d
Michael Harris <xero5k@yahoo.com>
2023-07-31 08:51:41 -0500

    Starting useContext

---
+++ client/src/context/user.js
@@ -0,0 +1,17 @@
+import React, { createContext } from "react";
+
+const UserContext = createContext("");
+
+function UserProvider({children}){
+
+
+//Creating UseContext
+
+return( 
+  <div></div>
+
+);
+
+}
+
+export { UserContext, UserProvider };
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/user';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-window.React1 = require('react');
+//window.React1 = require('react');
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <UserProvider>
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+  </UserProvider>
 ); 
 
 // If you want to start measuring performance in your app, pass a function
